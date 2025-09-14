a385970f31be3bfb3ca60da28d36e697ab352531
Dilraj06 <balnoor004@gmail.com>
2025-09-14 17:11:52 -0500

    added GitHub workflow for ci and fixed calculate PortfolioPerformance function

---
+++ src/portfolio/portfolioPerformance.ts
@@ -0,0 +1,52 @@
+interface PortfolioPerformance {
+    initialInvestment: number;
+    currentValue: number;
+    profitOrLoss: number;
+    percentageChange: number;
+    performanceSummary: string;
+}
+ 
+//
+export function calculatePortfolioPerformance(
+    initialInvestment: number,
+    currentValue: number
+):  PortfolioPerformance { 
+   
+    const profitOrLoss = currentValue - initialInvestment;
+ 
+    const percentageChange = (profitOrLoss / initialInvestment) * 100;
+    /**
+     * 
+     * @param percentageChange
+     * @returns 
+     */
+    const performanceSummary = (percentageChange: number): string => {
+    switch (true) {
+        case percentageChange >= 20:
+            return `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
+        case percentageChange >= 10 && percentageChange < 20: 
+            return `The portfolio has gained moderately with the profit of $${profitOrLoss}.`;
+        case percentageChange >= 0.1 && percentageChange < 10:
+            return `The portfolio has gained slightly with the profit of $${profitOrLoss}.`;
+        case percentageChange === 0:
+            return `There is no change in portfolio`;
+        case percentageChange >= -9.99 && percentageChange < 0:
+            return `The portfolio has lost slightly with the loss of $${profitOrLoss}.` ;   
+        case percentageChange >= -19.99 && percentageChange <= -10:
+            return `The portfolio has lost moderetaly with the loss of $${profitOrLoss}.`;
+        case percentageChange <= -20:
+            return `The portfolio has lost significantly with the loss of $${profitOrLoss}.`;
+        default:
+            return `No change in portfolio`;
+        }
+ 
+    };
+ 
+    return {
+        initialInvestment,
+        currentValue,
+        profitOrLoss,
+        percentageChange,
+        performanceSummary: performanceSummary(percentageChange),
+    };
+}
